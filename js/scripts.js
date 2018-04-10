a7501d1c9715261cace9ad02697e46488e069fbf
daryl <husbondo76@gmail.com>
2018-04-09 20:59:51 -0700

    Worked on var and alert

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var myHistory = "I've wrestled since i was 6 years old. My sons and I know WWE is fake but we watch it religously.";
+console.log(myHistory);
+confirm(myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue)
