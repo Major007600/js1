1df79c4e023ac9d1a39e74b0c33d4c0b5ab29915
daryl <husbondo76@gmail.com>
2018-04-11 17:04:03 -0700

    added my hobbies

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 var myHistory = "I've wrestled since i was 6 years old. My sons and I know WWE is fake but we watch it religously.";
+
 console.log(myHistory);
+
 confirm(myHistory);
+
 var myValue = confirm(myHistory);
+
 console.log(myValue)
+
+var myHobby = "For my hobbies, i love cars. I love modifing them and for form and function. I also a digital DJ. I love to be able to munipulate songs and make them do what i want.";
+
+console.log(myHobby);
+
+confirm(myHobby);
+
+var myValue = confirm(myHobby);
+
+console.log(myValue);
